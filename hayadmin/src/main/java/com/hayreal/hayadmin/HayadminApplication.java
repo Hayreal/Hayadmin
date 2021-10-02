cdfe791c62e2ab2264dd7b1aa422014ff1b10ea6
1506875977@qq.com <li7332561>
2021-10-02 17:21:19 +0800

    hayadmin初始化

---
+++ hayadmin/src/main/java/com/hayreal/hayadmin/HayadminApplication.java
@@ -0,0 +1,13 @@
+package com.hayreal.hayadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HayadminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HayadminApplication.class, args);
+    }
+
+}
---
+++ hayadmin/src/test/java/com/hayreal/hayadmin/HayadminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hayreal.hayadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HayadminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
